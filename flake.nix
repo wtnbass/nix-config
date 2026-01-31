083860fa5dff752e3b847a5433e3c8a45312b1ad
wtnbass <wtnbass@icloud.com>
2026-01-31 14:45:39 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+    nix-ld.url = "github:Mic92/nix-ld";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, nix-ld, home-manager, ... }@inputs: {
+    # The host with the hostname `nixos` will use this configuration
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "25.05";
+            wsl.enable = true;
+          }
+
+          nix-ld.nixosModules.nix-ld
+          { programs.nix-ld.dev.enable = true; }
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,99 @@
+{ config, pkgs, ... }:
+
+{
+  # home.username = "nixos";
+  # home.homeDirectory = "/home/nixos";
+
+  home.packages = with pkgs;[
+    nixd
+    helix
+    tmux
+    carapace
+    ripgrep
+    bat
+    fd
+    eza
+    fzf
+    jq
+    difftastic
+    gh
+    gitui
+    lazygit
+    yazi
+    nodejs
+    bun
+    pnpm
+    nodePackages.vscode-langservers-extracted
+    nodePackages.typescript-language-server
+    markdown-oxide
+    nodePackages.yaml-language-server
+    tombi
+    go
+    gopls
+  ];
+
+  home.stateVersion = "25.05";
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user.name = "wtnbass";
+      user.email = "wtnbass@icloud.com";
+      core.editor = "helix";
+      diff.external = "difft";
+      merge.ff = "false";
+      pull.ff = "only";
+
+      # Ref: https://blog.gitbutler.com/how-git-core-devs-configure-git
+      # clearly makes git better
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      diff.algorithm = "histogram";
+      diff.colorMoved = "plain";
+      diff.mnemonicPrefix = "true";
+      diff.renames = "true";
+      push.default = "simple";
+      push.autoSetupRemote = "true";
+      push.followTags = "true";
+      fetch.prune = "true";
+      fetch.pruneTags = "true";
+      fetch.all = "true";
+
+      # why the hell not?
+      help.autocorrect = "prompt";
+      commit.verbose = "true";
+      rerere.enabled = "true";
+      rerere.autoupdate = "true";
+      # core.excludesfile = ""~/.gitignore"";
+      rebase.autoSquash = "true";
+      rebase.autoStash = "true";
+      rebase.updateRefs = "true";
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        file-picker = {
+          hidden = false;
+          git-ignore = true;
+        };
+      };
+      keys.normal = {
+        space = { space = ":reload-all"; };
+      };
+    };
+  };
+}
