e8b08361598b84922b7810fff2d79dbfb35fd08c
wtnbass <wtnbass@icloud.com>
2026-02-09 01:36:36 +0000

    starship

--- home.nix
+++ home.nix
@@ -86,6 +86,142 @@
 
   programs.starship = {
     enable = true;
+    settings = {
+      format = builtins.concatStringsSep "" [
+        "$os "
+        "$directory"
+        "$git_branch"
+        "$git_status"
+        "$nix_shell"
+        "$nodejs"
+        "$bun"
+        "$golang"
+        "$python"
+        "$rust"
+        "$java"
+        "$kotlin"
+        "$lua"
+        "$fill"
+        "$cmd_duration"
+        "$line_break"
+        "$character"
+      ];
+
+      palette = "kanagawa_wave";
+
+      palettes.kanagawa_wave = {
+        color_blue = "#7E9CD8"; # crystalBlue
+        color_green = "#98BB6C"; # springGreen
+        color_spring_blue = "#7FB4CA"; # springBlue
+        color_violet = "#957FB8"; # oniViolet
+        color_orange = "#FFA066"; # surimiOrange
+        color_red = "#FF5D62"; # peachRed
+        color_gray = "#727169"; # fujiGray
+      };
+
+      os = {
+        disabled = false;
+        style = "fg:color_violet";
+        symbols = {
+          NixOS = " ";
+          Arch = " ";
+          Ubuntu = " ";
+          Debian = " ";
+          Macos = " ";
+          Windows = "󰍲 ";
+          Linux = " ";
+        };
+      };
+
+      directory = {
+        style = "bold fg:color_blue";
+        read_only = " 󰌾";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+
+      git_branch = {
+        symbol = " ";
+        style = "fg:color_green";
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+      };
+
+      git_status = {
+        style = "fg:color_red";
+        format = "[$all_status$ahead_behind]($style)";
+      };
+
+      nix_shell = {
+        symbol = " ";
+        style = "fg:color_spring_blue";
+        format = "[$symbol$state]($style) ";
+      };
+
+      nodejs = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      bun = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      golang = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      python = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      rust = {
+        symbol = "󱘗 ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      java = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      kotlin = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      lua = {
+        symbol = " ";
+        style = "fg:color_orange";
+        format = "[$symbol$version]($style) ";
+      };
+
+      fill.symbol = " ";
+
+      cmd_duration = {
+        style = "fg:color_gray";
+        format = " $duration";
+        min_time = 3000;
+      };
+
+      character = {
+        success_symbol = "[❯](bold fg:color_green)";
+        error_symbol = "[❯](bold fg:color_red)";
+        vimcmd_symbol = "[❮](bold fg:color_green)";
+        vimcmd_replace_one_symbol = "[❮](bold fg:color_violet)";
+        vimcmd_replace_symbol = "[❮](bold fg:color_violet)";
+        vimcmd_visual_symbol = "[❮](bold fg:color_blue)";
+      };
+    };
   };
 
   programs.git = {
