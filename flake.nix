a4638fe78731508268a1dfe88530597a3dc09d00
wtnbass <wtnbass@icloud.com>
2026-05-24 10:48:32 +0900

    add cache.nixos.org

--- flake.nix
+++ flake.nix
@@ -1,7 +1,14 @@
 {
   nixConfig = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];  };
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.numtide.com"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" 
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
